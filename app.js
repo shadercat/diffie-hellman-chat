e5ca0c03e20081ee7ae89dcd0dac50c957287d15
shadercat <shadercatin@gmail.com>
2020-05-10 03:48:09 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const generator = require('./functions');
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+server = app.listen(3000);
+
+//socket.io instantiation
+const io = require("socket.io")(server);
+
+const publicSecret = configurePublicKeys();
+let clients = new Set();
+
+//listen on every connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.emit('get_public_key', publicSecret);
+
+    clients.add(socket);
+    let s = shareKeys();
+
+    //default username
+    socket.username = "Anonymous";
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    });
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message: data.message, username: socket.username});
+    });
+
+    //listen on typing
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', {username: socket.username});
+    });
+
+    socket.on('disconnect', () => {
+        io.sockets.emit('new_message', {message: `${socket.username} disconnected...`, username: 'server'});
+        clients.delete(socket);
+    })
+});
+
+async function shareKeys() {
+    for (let client of clients) {
+        let middleMess = undefined;
+        for (let middle of clients) {
+            if (client !== middle) {
+                await compute(middle, middleMess).then((data) => {
+                    middleMess = data;
+                });
+            }
+        }
+        if(middleMess !== undefined){
+            client.emit('send_mess', middleMess);
+        }
+    }
+    return Promise.resolve(1);
+}
+
+function compute(socket, middleMess) {
+    return new Promise((resolve, reject) => {
+        let timer;
+        socket.once('get_mess', responseHandler);
+        function responseHandler(data) {
+            clearTimeout(timer);
+            resolve(data);
+        }
+        timer = setTimeout(() => {
+            reject("Waiting timeout");
+        }, 10000);
+        socket.emit('get_mess', middleMess);
+    })
+}
+
+function configurePublicKeys() {
+    let prime = generator.randomPrime();
+    let g = generator.gGenerator(prime);
+    console.log("Configure public keys end.");
+    return {
+        p: prime,
+        g: g
+    }
+}
---
+++ public/chat.js
@@ -0,0 +1,85 @@
+
+$(function(){
+   	//make connection
+	let socket = io.connect('http://localhost:3000');
+
+	//buttons and inputs
+	let message = $("#message");
+	let username = $("#username");
+	let send_message = $("#send_message");
+	let send_username = $("#send_username");
+	let chatroom = $("#chatroom");
+	let feedback = $("#feedback");
+
+	let publicKey = {
+		p: 0,
+		g: 0
+	};
+
+	let secretKey = Math.floor(Math.random() * (1000 - 1)) + 1;
+	let sharedSecret = 0;
+
+
+	socket.on('get_public_key', (data) => {
+		publicKey = data;
+		console.log('public key');
+		console.log(publicKey);
+		console.log("secret key: " + secretKey);
+	});
+
+	socket.on('get_mess', (data) => {
+		let num = 1;
+		if(data !== null){
+			for(let i = 0; i < secretKey; i++){
+				num *= data;
+				num %= publicKey.p;
+			}
+		} else {
+			for (let i = 0; i < secretKey; i++){
+				num *= publicKey.g;
+				num %= publicKey.p;
+			}
+		}
+		socket.emit('get_mess', num);
+	});
+
+	socket.on('send_mess', (data) => {
+		// sharedSecret = Math.pow(data, secretKey) % publicKey.p;
+		//sharedSecret = BigInt(data) ** BigInt(secretKey) % BigInt(publicKey.p);
+		sharedSecret = 1;
+		for(let i = 0; i < secretKey; i++){
+			sharedSecret *= data;
+			sharedSecret %= publicKey.p;
+		}
+		console.log(`shared secret: ${sharedSecret}`);
+	});
+
+	//Emit message
+	send_message.click(function(){
+		socket.emit('new_message', {message : message.val()})
+	});
+
+	//Listen on new_message
+	socket.on("new_message", (data) => {
+		feedback.html('');
+		message.val('');
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+	});
+
+	//Emit a username
+	send_username.click(function(){
+		socket.emit('change_username', {username : username.val()})
+	});
+
+	//Emit typing
+	message.bind("keypress", () => {
+		socket.emit('typing')
+	});
+
+	//Listen on typing
+	socket.on('typing', (data) => {
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+	});
+});
+
+
